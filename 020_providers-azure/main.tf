57f6a499bc3ab6146d42d8a857ec1bb3ce4417f1
bigonil <luca.bigoni@gmail.com>
2025-01-23 10:46:17 +0100

    020_providers-azure/main.tf fix terraform module path

--- 020_providers-azure/main.tf
+++ 020_providers-azure/main.tf
@@ -1,29 +1,20 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "2.77.0"
+      source  = "hashicorp/azurerm"
+      version = ">= 3.11.0, < 4.0.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+  subscription_id = "8d620ffe-f57d-4b15-b5b8-7acff17b1869"
 }
 
 resource "azurerm_resource_group" "terraform_azure_providers" {
-	name = "terraform_azure_providers"
-	location = "East US"
-}
-
-module "linuxservers" {
-  source              = "Azure/compute/azurerm"
-  resource_group_name = azurerm_resource_group.terraform_azure_providers.name
-  vm_os_simple        = "UbuntuServer"
-  public_ip_dns       = ["linsimplevmips"] // change to a unique name per datacenter region
-  vnet_subnet_id      = module.network.vnet_subnets[0]
-	vm_size             = "Standard_B1ls"
-  depends_on = [azurerm_resource_group.terraform_azure_providers]
+  name     = "terraform_azure_providers"
+  location = "East US"
 }
 
 module "network" {
@@ -31,10 +22,29 @@ module "network" {
   resource_group_name = azurerm_resource_group.terraform_azure_providers.name
   subnet_prefixes     = ["10.0.1.0/24"]
   subnet_names        = ["subnet1"]
+  use_for_each        = false
 
   depends_on = [azurerm_resource_group.terraform_azure_providers]
 }
 
-output "linux_vm_public_name" {
-  value = module.linuxservers.public_ip_dns_name
+module "linuxservers" {
+  source              = "Azure/compute/azurerm"
+  resource_group_name = azurerm_resource_group.terraform_azure_providers.name
+  vm_os_simple        = "UbuntuServer"
+  public_ip_dns = ["linsimplevmips"] # Unique name for the public IP
+  vnet_subnet_id      = module.network.vnet_subnets[0]
+
+  depends_on = [azurerm_resource_group.terraform_azure_providers]
+}
+
+module "windowsservers" {
+  source              = "Azure/compute/azurerm"
+  resource_group_name = azurerm_resource_group.terraform_azure_providers.name
+  is_windows_image    = true
+  vm_hostname         = "mywinvm"
+  admin_password      = "ComplxP@ssw0rd!"
+  vm_os_simple        = "WindowsServer"
+  public_ip_dns = ["winsimplevmips"] # Unique name for the public IP
+  vnet_subnet_id      = module.network.vnet_subnets[0]
+  depends_on = [azurerm_resource_group.terraform_azure_providers]
 }
