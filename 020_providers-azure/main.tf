9d735ef2f82fb43d15c5ea7424d3303a3d636d1e
bigonil <luca.bigoni@gmail.com>
2025-01-25 19:37:51 +0100

    Fix typo in main.tf for 020_providers-azure

--- 020_providers-azure/main.tf
+++ 020_providers-azure/main.tf
@@ -9,7 +9,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "8d620ffe-f57d-4b15-b5b8-7acff17b1869"
 }
 
 resource "azurerm_resource_group" "terraform_azure_providers" {
@@ -19,32 +18,27 @@ resource "azurerm_resource_group" "terraform_azure_providers" {
 
 module "network" {
   source              = "Azure/network/azurerm"
+  version             = ">= 2.1.0" # Pin the module version
   resource_group_name = azurerm_resource_group.terraform_azure_providers.name
   subnet_prefixes     = ["10.0.1.0/24"]
   subnet_names        = ["subnet1"]
-  use_for_each        = false
+
+  use_for_each = false
 
   depends_on = [azurerm_resource_group.terraform_azure_providers]
 }
 
 module "linuxservers" {
   source              = "Azure/compute/azurerm"
+  version             = "3.2.0" # Pin the module version
   resource_group_name = azurerm_resource_group.terraform_azure_providers.name
   vm_os_simple        = "UbuntuServer"
-  public_ip_dns = ["linsimplevmips"] # Unique name for the public IP
-  vnet_subnet_id      = module.network.vnet_subnets[0]
-
-  depends_on = [azurerm_resource_group.terraform_azure_providers]
+  public_ip_dns       = ["linsimplevmip"] # List with one string
+  vnet_subnet_id      = module.network.vnet_subnets["subnet1"] # Map key reference
+  vm_size             = "Standard_B1ls"
+  depends_on          = [azurerm_resource_group.terraform_azure_providers]
 }
 
-module "windowsservers" {
-  source              = "Azure/compute/azurerm"
-  resource_group_name = azurerm_resource_group.terraform_azure_providers.name
-  is_windows_image    = true
-  vm_hostname         = "mywinvm"
-  admin_password      = "ComplxP@ssw0rd!"
-  vm_os_simple        = "WindowsServer"
-  public_ip_dns = ["winsimplevmips"] # Unique name for the public IP
-  vnet_subnet_id      = module.network.vnet_subnets[0]
-  depends_on = [azurerm_resource_group.terraform_azure_providers]
+output "linux_vm_public_name" {
+  value = module.linuxservers.public_ip_dns # Correct attribute name
 }
