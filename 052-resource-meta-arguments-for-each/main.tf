e23e6544be154c57218621a2b8ea60d54fd33b6a
bigonil <luca.bigoni@gmail.com>
2025-03-18 12:44:06 +0100

    Update main.tf and output.tf

--- 052-resource-meta-arguments-for-each/main.tf
+++ 052-resource-meta-arguments-for-each/main.tf
@@ -12,14 +12,14 @@ provider "aws" {
   region  = "us-east-1"
 }
 resource "aws_instance" "my_server" {
-	for_each = {
-		nano = "t2.nano"
-		micro =  "t2.micro"
-		small =  "t2.small"
-	}
+  for_each = {
+    nano  = "t2.nano"
+    micro = "t2.micro"
+    small = "t2.small"
+  }
   ami           = "ami-087c17d1fe0178315"
   instance_type = each.value
-	tags = {
-		Name = "Server-${each.key}"
-	}
+  tags = {
+    Name = "Server-${each.key}"
+  }
 }
