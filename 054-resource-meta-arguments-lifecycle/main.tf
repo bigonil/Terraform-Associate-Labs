3fe842beebe90893d2d3ce70081d767912e44f47
bigonil <luca.bigoni@gmail.com>
2025-03-20 22:11:39 +0100

    Update Lifecycle Arguments

--- 054-resource-meta-arguments-lifecycle/main.tf
+++ 054-resource-meta-arguments-lifecycle/main.tf
@@ -2,24 +2,24 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.58.0"
+      version = ">= 5.90.0"
     }
   }
 }
 
 provider "aws" {
-  profile = "default"
+  profile = "lb-aws-admin"
   region  = "us-east-1"
 }
 resource "aws_instance" "my_server" {
   ami           = "ami-087c17d1fe0178315"
   instance_type = "t2.micro"
-	tags = {
-		Name = "My-Server"
-	}
-	lifecycle {
-		prevent_destroy = false
-	} 
+  tags = {
+    Name = "My-Server"
+  }
+  lifecycle {
+    prevent_destroy = false
+  }
 }
 
 output "public_ip" {
