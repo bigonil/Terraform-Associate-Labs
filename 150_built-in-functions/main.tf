f8e89a89bb2ae2b5c5236e7b217e2d42550bf392
Bigoni Luca <luca.bigoni@gruppohera.it>
2025-06-14 19:25:30 +0200

    Update manifest for 150_built-in-functions

--- 150_built-in-functions/main.tf
+++ 150_built-in-functions/main.tf
@@ -18,3 +18,57 @@ variable "stuff" {
 		"goodbye" = "moon"
 	}
 }
+
+output "now" {
+  value = timestamp()
+}
+output "upper" {
+  value = upper(var.str)
+}
+
+output "formatted_date" {
+  value = formatdate("YYYY-MM-DD", timestamp())
+}
+output "join_items" {
+  value = join("-", var.items)
+}
+output "lookup_stuff" {
+  value = lookup(var.stuff, "hello", "default_value")
+}
+output "coalesce_items" {
+  value = coalesce(var.items...)
+}
+output "coalesce_stuff" {
+  value = coalesce(var.stuff["hello"], var.stuff["goodbye"], "default_value")
+}
+output "coalesce_empty" {
+  value = coalesce("", null, "default_value")
+}
+output "coalesce_empty_list" {
+  value = coalesce([], null, ["default_value"])
+}
+output "coalesce_empty_map" {
+  value = coalesce({}, null, {"default_key" = "default_value"})
+}
+output "coalesce_empty_string" {
+  value = coalesce("", null, "default_value")
+}
+output "coalesce_empty_string_list" {
+  value = coalesce([""], null, ["default_value"])
+}
+output "coalesce_empty_string_map" {
+  value = coalesce({"key" = ""}, null, {"default_key" = "default_value"})
+}
+output "coalesce_empty_string_map_with_default" {
+  value = coalesce({"key" = ""}, null, {"default_key" = "default_value"}, "fallback_value")
+}
+output "coalesce_empty_string_map_with_default_list" {
+  value = coalesce({"key" = ""}, null, {"default_key" = "default_value"}, ["fallback_value"])
+}
+output "coalesce_empty_string_map_with_default_map" {
+  value = coalesce({"key" = ""}, null, {"default_key" = "default_value"}, {"fallback_key" = "fallback_value"})
+}
+output "coalesce_empty_string_map_with_default_map_and_list" {
+  value = coalesce({"key" = ""}, null, {"default_key" = "default_value"}, {"fallback_key" = "fallback_value"}, ["fallback_value"])
+}
+
